6fc7fcc9d36685b105a6a90e07a6b9909bf96357
rigorio <rigorei.sarmiento@gmail.com>
2023-07-29 14:05:07 +0800

    Initial commit

---
+++ src/main/java/io/rigor/showcase/JavaShowcaseApplication.java
@@ -0,0 +1,13 @@
+package io.rigor.showcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaShowcaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaShowcaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/rigor/showcase/JavaShowcaseApplicationTests.java
@@ -0,0 +1,13 @@
+package io.rigor.showcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaShowcaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
